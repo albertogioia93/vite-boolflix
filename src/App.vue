9648cce6a87df2d8408ccb10d55d18e1f45bcd0c
albertogioia93 <albertogioia@hotmail.it>
2023-03-29 16:36:42 +0200

    scaffolding + chiarimenti milestone 0

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
 <script>
+// LIBRERIE
+import axios from 'axios';
+
+// FUNZIONI
+import { store } from './store';
 import HelloApp from './components/HelloApp.vue';
 export default {
   name: 'App',
   components: {
     HelloApp
+  },
+  data(){
+    return {
+      store
+    }
+  },
+  created() {
+        // axios.get('inserisci indirizzo della API')
+        axios.get('https://api.themoviedb.org/3/search/movie?api_key=b01fec227b714470bbc304ad4ef56c14')
+            .then ((response) => {
+                console.log(response);
+                // const characters = response.data.data.splice(0, 100);
+                // this.store.characters = characters
+                // this.store.charactersFound = characters.length
+            });
+
   }
 }
 </script>
