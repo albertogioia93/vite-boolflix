bdd74fbf77d92e62d76630406b0a27bf37aa8d4c
albertogioia93 <albertogioia@hotmail.it>
2023-03-30 17:30:48 +0200

    flags visualizzate su info dei film post ricerca

--- src/components/HelloApp.vue
+++ src/components/HelloApp.vue
@@ -1,8 +1,26 @@
 <script>
+// dopo aver installato dal terminale il pacchetto delle flags preso sul sito npm, lo importo nel componente e lo esporto in components
+import CountryFlag from 'vue-country-flag-next'
     export default {
         name: 'Hello',
+        components: {
+            CountryFlag
+        },
         props: {
             info: Object
+        },
+        // creo una funzione per il caso delle lingue uguali parlate in più luoghi
+        computed: {
+            getLanguage() {
+                switch (this.info.original_language) {
+                    case 'en':
+                        return 'gb';
+                    case 'he':
+                        return 'il';
+                    default:
+                        return this.info.original_language
+                }
+            }
         }
     }
 </script>
@@ -11,7 +29,9 @@
     <article>
         <h3>{{ info.title }}</h3>
         <h4>Titolo originale: {{ info.original_title }}</h4>
-        <div>Lingua: {{ info.original_language }}</div>
+        <!-- con quella funzione le bandierine del pacchetto delle flags si adattano al luogo switchato dalla lingua -->
+        <div>Lingua: {{ getLanguage }}</div>
+        <country-flag :country='getLanguage' size='normal'/>
         <div>Voto: {{ info.vote_average }}</div>
     </article>
 </template>
