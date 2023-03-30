51a58de63a2177bceb49fb73b152afc7a62da897
albertogioia93 <albertogioia@hotmail.it>
2023-03-30 18:22:26 +0200

    installazione font awesome

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import './assets/scss/main.scss';
 import App from './App.vue'
-createApp(App).mount('#app')
+
+// FONT AWESOME
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faHatWizard } from '@fortawesome/free-solid-svg-icons'
+library.add(faHatWizard)
+
+
+
+createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
